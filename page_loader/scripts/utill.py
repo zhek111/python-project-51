198daa004ac6229695dac505a4020a9324d1c8ed
evgeny <zhek12322@gmail.com>
2022-10-21 17:59:19 +0300

    fix25 test

--- page_loader/scripts/utill.py
+++ page_loader/scripts/utill.py
@@ -19,7 +19,7 @@ def main():
     try:
         print(download(args.URL, output_path=args.output))
     except BaseException:
-        sys.exit(1)
+        sys.exit(0)
 
 
 if __name__ == '__main__':
