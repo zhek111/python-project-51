87f3577dfa00a9ebf733fbe49d82c77900b2f00c
evgeny <zhek12322@gmail.com>
2022-10-24 10:55:27 +0300

    fix33 test

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,4 +2,4 @@ import os
 
 
 def get_path(file, *dir):
-    return os.path.join('tests', 'fixtures', *dir, file)
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures', *dir, file)
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,7 +1,7 @@
 
 import tempfile
 
-
+from tests import get_path
 import pytest
 import requests
 
@@ -28,75 +28,75 @@ def test_error_download2(requests_mock):
         download('https://ru.hexlettt.io/courses')
 
 
-# def test_download(requests_mock, tempp):
-#     with tempp as tmp:
-#         url_1 = open(
-#         get_path('ru-hexlet-io-lessons.rss', 'ru-hexlet-io-courses_files'),
-#             'rb')
-#         data1 = url_1.read()
-#         requests_mock.get(
-#             'https://ru.hexlet.io/about/lessons.rss',
-#             content=data1)
-#         file_with_download = open(get_path('with_download.html'))
-#         correct_data = file_with_download.read()
-#         initial_file = open(get_path('initial.html'))
-#         initial_data = initial_file.read()
-#         requests_mock.get('https://ru.hexlet.io/courses', text=initial_data)
-#         path_download_file = download('https://ru.hexlet.io/courses', tmp)
-#         download_file = open(path_download_file)
-#         expect_data = download_file.read()
-#         assert expect_data == correct_data
+def test_download(requests_mock, tempp):
+    with tempp as tmp:
+        url_1 = open(
+        get_path('ru-hexlet-io-lessons.rss', 'ru-hexlet-io-courses_files'),
+            'rb')
+        data1 = url_1.read()
+        requests_mock.get(
+            'https://ru.hexlet.io/about/lessons.rss',
+            content=data1)
+        file_with_download = open(get_path('with_download.html'))
+        correct_data = file_with_download.read()
+        initial_file = open(get_path('initial.html'))
+        initial_data = initial_file.read()
+        requests_mock.get('https://ru.hexlet.io/courses', text=initial_data)
+        path_download_file = download('https://ru.hexlet.io/courses', tmp)
+        download_file = open(path_download_file)
+        expect_data = download_file.read()
+        assert expect_data == correct_data
 
 
-# def test_download2(requests_mock, tempp):
-#     with tempp as tmp:
-#         url_1 = open(get_path(
-#             'localhost-blog-about-assets-styles.css',
-#             'expected',
-#             'localhost-blog-about_files'),
-#             'rb')
-#         data1 = url_1.read()
-#         requests_mock.get(
-#             'https://localhost/blog/about/assets/styles.css',
-#             content=data1)
-#         url_2 = open(get_path(
-#             'localhost-blog-about.html',
-#             'expected',
-#             'localhost-blog-about_files'),
-#             'rb')
-#         data2 = url_2.read()
-#         requests_mock.get(
-#             'https://localhost/blog/about',
-#             content=data2)
-#         url_3 = open(get_path(
-#             'localhost-photos-me.jpg',
-#             'expected',
-#             'localhost-blog-about_files'),
-#             'rb')
-#         data3 = url_3.read()
-#         requests_mock.get(
-#             'https://localhost/photos/me.jpg',
-#             content=data3)
-#         url_4 = open(get_path(
-#             'localhost-assets-scripts.js',
-#             'expected',
-#             'localhost-blog-about_files'),
-#             'rb')
-#         data4 = url_4.read()
-#         requests_mock.get(
-#             'http://localhost/assets/scripts.js',
-#             content=data4)
-#         file_with_download = open(get_path(
-#             'localhost-blog-about.html',
-#             'expected'))
-#         correct_data = file_with_download.read()
-#         initial_file2 = open(get_path('localhost-blog-about.html'))
-#         initial_data2 = initial_file2.read()
-#         requests_mock.get('https://localhost/blog/about', text=initial_data2)
-#         path_download_file = download('https://localhost/blog/about', tmp)
-#         download_file = open(path_download_file)
-#         expect_data = download_file.read()
-#         assert expect_data == correct_data
+def test_download2(requests_mock, tempp):
+    with tempp as tmp:
+        url_1 = open(get_path(
+            'localhost-blog-about-assets-styles.css',
+            'expected',
+            'localhost-blog-about_files'),
+            'rb')
+        data1 = url_1.read()
+        requests_mock.get(
+            'https://localhost/blog/about/assets/styles.css',
+            content=data1)
+        url_2 = open(get_path(
+            'localhost-blog-about.html',
+            'expected',
+            'localhost-blog-about_files'),
+            'rb')
+        data2 = url_2.read()
+        requests_mock.get(
+            'https://localhost/blog/about',
+            content=data2)
+        url_3 = open(get_path(
+            'localhost-photos-me.jpg',
+            'expected',
+            'localhost-blog-about_files'),
+            'rb')
+        data3 = url_3.read()
+        requests_mock.get(
+            'https://localhost/photos/me.jpg',
+            content=data3)
+        url_4 = open(get_path(
+            'localhost-assets-scripts.js',
+            'expected',
+            'localhost-blog-about_files'),
+            'rb')
+        data4 = url_4.read()
+        requests_mock.get(
+            'http://localhost/assets/scripts.js',
+            content=data4)
+        file_with_download = open(get_path(
+            'localhost-blog-about.html',
+            'expected'))
+        correct_data = file_with_download.read()
+        initial_file2 = open(get_path('localhost-blog-about.html'))
+        initial_data2 = initial_file2.read()
+        requests_mock.get('https://localhost/blog/about', text=initial_data2)
+        path_download_file = download('https://localhost/blog/about', tmp)
+        download_file = open(path_download_file)
+        expect_data = download_file.read()
+        assert expect_data == correct_data
 
 
 # def test_download3(requests_mock, tempp):
