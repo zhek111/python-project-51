1f2859ce1ef51b3970219792f016d29d7f2f83f6
evgeny <zhek12322@gmail.com>
2022-10-24 18:28:42 +0300

    fix41 test

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -14,9 +14,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 def get_name_data(path, url, dir=None):
     aaa = urlparse(urljoin(path, url))
-    full_path = aaa.netloc + aaa.path
+    logging.debug(aaa)
+    full_path = aaa.netloc + aaa.path + aaa.params + aaa.query + aaa.fragment
+    logging.debug(full_path)
     name_file_without_extension = re.sub(r'\W', '-',
                                          splitext(full_path)[0])
+    logging.debug(name_file_without_extension)
     if dir:
         return f'{name_file_without_extension}_files'
     if splitext(full_path)[1]:
@@ -65,7 +68,9 @@ def download(path, output_path=os.getcwd()):
         logging.debug('GGGGGGGTTTGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG')
         raise
     full_path_page = os.path.join(output_path, get_name_data(path, path))
+    logging.debug(full_path_page)
     name_dir = os.path.join(output_path, get_name_data(path, path, dir=True))
+    logging.debug(name_dir)
     response = requests.get(path)
     soup = BeautifulSoup(response.content, 'html.parser')
     nessasary_tags = soup.find_all(['img', 'link', 'script'])
@@ -74,14 +79,19 @@ def download(path, output_path=os.getcwd()):
         atrs = get_atrs(teg)
         if check_domain(path, atrs.get('url')):
             necessary_urls.append(atrs)
+            logging.debug(atrs)
     for link in nessasary_tags:
         url = get_atrs(link).get('url')
+        logging.debug(url)
         if check_domain(path, url):
             if not os.path.isdir(name_dir):
+                logging.debug(name_dir)
                 os.mkdir(name_dir)
             for i in Bar('Processing').iter(necessary_urls):
                 data = requests.get(urljoin(path, url))
             name_file = get_name_data(path, url)
+            print('NAME FILE', name_file)
+            print('NAME DIR', name_dir)
             with open(os.path.join(name_dir, name_file), 'wb') as file:
                 file.write(data.content)
             link[get_atrs(link).get('atr')] = os.path.join(
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,4 @@
+
 import tempfile
 
 from tests import get_path
@@ -93,6 +94,58 @@ def test_download2(requests_mock, tempp):
         initial_data2 = initial_file2.read()
         requests_mock.get('https://localhost/blog/about', text=initial_data2)
         path_download_file = download('https://localhost/blog/about', tmp)
+        download_file = open(path_download_file)
+        expect_data = download_file.read()
+        assert expect_data == correct_data
+
+
+def test_download3(requests_mock, tempp):
+    with tempp as tmp:
+        url_1 = open(get_path(
+            'site-com-blog-about-assets-styles.css',
+            'expected',
+            'site-com-blog-about_files'),
+            'rb')
+        data1 = url_1.read()
+        requests_mock.get(
+            'https://site.com/blog/about/assets/styles.css',
+            content=data1)
+        url_2 = open(get_path(
+            'site-com-blog-about.html',
+            'expected',
+            'site-com-blog-about_files'),
+            'rb')
+        data2 = url_2.read()
+        requests_mock.get(
+            'https://site.com/blog/about',
+            content=data2)
+        url_3 = open(get_path(
+            'site-com-photos-me.jpg',
+            'expected',
+            'site-com-blog-about_files'),
+            'rb')
+        data3 = url_3.read()
+        requests_mock.get(
+            'https://site.com/photos/me.jpg',
+            content=data3)
+        url_4 = open(get_path(
+            'site-com-assets-scripts.js',
+            'expected',
+            'site-com-blog-about_files'),
+            'rb')
+        data4 = url_4.read()
+        requests_mock.get(
+            'https://site.com/assets/scripts.js',
+            content=data4)
+        file_with_download = open(get_path(
+            'site-com-blog-about.html',
+            'expected'))
+        correct_data = file_with_download.read()
+        initial_file3 = open(get_path(
+            'site-com-blog-about.html'))
+        initial_data3 = initial_file3.read()
+        requests_mock.get('https://site.com/blog/about', text=initial_data3)
+        path_download_file = download('https://site.com/blog/about', tmp)
         download_file = open(path_download_file)
         expect_data = download_file.read()
         assert expect_data == correct_data
